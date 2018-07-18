0ac1275a4a7827af8df6717ed6f424a5947060fa
Bruno Agutoli <bruno.agutoli@gmail.com>
2018-07-18 12:47:52 -0300

    removed console.log

--- handler.js
+++ handler.js
@@ -4,6 +4,5 @@ const HttpHandler = require('./src/HttpHandler')
 
 module.exports.index = async (event, context, callback) => {
   const res = await HttpHandler(event)
-  console.log(res, 'asdasdas');
   callback(null, res)
 }
